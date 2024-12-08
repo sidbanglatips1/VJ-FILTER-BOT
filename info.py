f28581c9ccb2a9c121155fe3a62504e70312a64c
sidbanglatips1 <167536055+sidbanglatips1@users.noreply.github.com>
2024-12-08 18:32:34 +0400

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://cool-abigale-mongo123-bf01efd1.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
